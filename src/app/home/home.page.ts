ba42ffc6d399d476dcea4b36afd5d6de7ce36618
victor hugo michel duran <victor@keybest.com.br>
2019-07-26 11:58:12 -0300

    Aula 26 07

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario, UsuarioLogado } from '../model/usuario';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   //nome_variavel: tipo;
   pedidos: number = 25;
+  //usuario logado
+  usuarioLogado: Usuario;
+  usuarioLogadoNaClasse: UsuarioLogado;
+  usuarios: UsuarioLogado[] = []; //[usuario1,usuario2.....]
   constructor(private router: Router) {
 
   }
 
   // teste 
+
+  ngOnInit(): void {
+    this.usuarioLogadoNaClasse = new UsuarioLogado('11:39', 'xyz');
+    this.usuarioLogadoNaClasse.email = 'victor@keybest';
+
+    
+    let usuarioLogadoNaClasse2 = new UsuarioLogado('11:40', 'xyz');
+    usuarioLogadoNaClasse2.email = 'marcelo@keybest';
+
+    let usuarioLogadoNaClasse3 = new UsuarioLogado('11:00', 'xyz');
+    usuarioLogadoNaClasse3.email = 'raimo@keybest';
+
+    this.usuarios.push(this.usuarioLogadoNaClasse);
+    this.usuarios.push(usuarioLogadoNaClasse2);
+    this.usuarios.push(usuarioLogadoNaClasse3);
+    /*
+    this.usuarioLogado = { 
+      id: 1, 
+      nome: 'Victor', 
+      email: 'victor@keybest.com.br',   
+      administrador: true      
+    };
+*/
+  }
+  
   
   addPedido() {
     var tmp = this.pedidos;
