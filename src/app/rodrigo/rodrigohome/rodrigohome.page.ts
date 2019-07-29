9c42fc716d9065c7e8fc5968d3f412d500fe5267
victor hugo michel duran <victor@keybest.com.br>
2019-07-29 17:07:21 -0300

    Aula 29 07

--- src/app/rodrigo/rodrigohome/rodrigohome.page.ts
+++ src/app/rodrigo/rodrigohome/rodrigohome.page.ts
@@ -8,16 +8,35 @@ import { Router } from '@angular/router';
 })
 export class RodrigohomePage implements OnInit {
 
+  //escopo global. de classe
+  total: number = 0;
+  incremento: string = '0';
+
   constructor(private router: Router) {
   }
 
   ngOnInit() {
+  }  
+
+  somarClick(): void {
+    this.total = this.total + this.somar(10, parseInt(this.incremento));
+  }
+
+  //parametros num1, num2
+  //escopo local, de função
+  somar(num1: number, num2: number): number {
+    let totalF: number;
+    totalF = num1 + num2;
+    return totalF;
   }
 
   paginaMarcelo() {
+    this.total = 0;
+
     console.log('Ir para a página do Marcelo');
     this.router.navigateByUrl('marcelo-home');
   }  
+
   paginaRaimo(){
     console.log('Ira para a página do Raimo');
     this.router.navigateByUrl('raimo-home');
