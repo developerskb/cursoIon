deee4e2a0dda151cf7988adef17ed4721409ed33
Raimo6 <raimo@keybest.com.br>
2019-07-30 16:07:28 -0300

    Fla

--- src/app/raimo/raimo-home/raimo-home.page.ts
+++ src/app/raimo/raimo-home/raimo-home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-raimo-home',
@@ -7,14 +7,46 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./raimo-home.page.scss'],
 })
 export class RaimoHomePage implements OnInit {
+  //escopo global. de classe
+  total: number = 0;
+  incremento: string = '0';
 
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {    
-    //ler o valor passado na url. ex raimo-home/flamengo
-    //o valor do meuTime será 'flamengo'
-    const meuTime = this.activatedRoute.snapshot.paramMap.get('time');    
-    console.log('Meu time é: ', meuTime);
+  constructor(private router: Router) {
   }
+
+  ngOnInit() {
+  }  
+
+  somarClick(): void {
+    this.total = this.total + this.somar(10, parseInt(this.incremento));
+  }
+
+  //parametros num1, num2
+  //escopo local, de função
+  somar(num1: number, num2: number): number {
+    let totalF: number;
+    totalF = num1 + num2;
+    return totalF;
+  }
+  paginaRodrigo() {
+    console.log('ir para a pagina do rodrigo');
+    this.router.navigateByUrl('rodrigohome');
+  }
+
+  paginaMarcelo() {
+    this.total = 0;
+
+    console.log('Ir para a página do Marcelo');
+    this.router.navigateByUrl('marcelo-home');
+  }
+  paginaVictor(){
+    console.log('Ira para a página do Victor');
+    this.router.navigateByUrl('victor-home');
+  }
+  paginaRobson(){
+    console.log('Ir para página do Robson');
+    this.router.navigateByUrl('robsonHome');
+  }  
+  
 
 }
