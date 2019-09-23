718bb41e62d81723365a199087a92a0e7f8251c2
Raimo6 <raimo@keybest.com.br>
2019-09-23 15:30:48 -0300

    validação 50%

--- src/app/raimo/raimo-funcionario/raimo-funcionario.page.ts
+++ src/app/raimo/raimo-funcionario/raimo-funcionario.page.ts
@@ -20,17 +20,27 @@ export class RaimoFuncionarioPage implements OnInit {
     //this.nome = 'do banco de dados';
     this.usuario = new UsuarioLogado(null, null);
     //this.usuario.nome = 'teste'
+    this.usuario.sexo = 'M';
     this.estados = this.utils.listarEstados();
 
 
-    //usario raimo
+    //usuario raimo
     console.log(this.utils.eImpar(99));
     console.log(this.estados);
   }
   validar() {
     console.log('meu usuario:', this.usuario);
     if (this.utils.campoVazio(this.usuario.nome)) {
-      this.utils.showAlert('Informe o Nome');
+      this.utils.showAlert('Informe o Nome', "Campo Vazio!");
+    }  
+    if (this.utils.campoVazio(this.usuario.dataNascimento)) {
+      this.utils.showAlert("Informe a Data de Nascimento", "Campo Vazio!");
     }
+    if (this.utils.campoVazio(this.usuario.email)) {
+      this.utils.showAlert('Informe o Email', "Campo Vazio!");
+    }
+    if (this.utils.campoVazio(this.usuario.estado)) {
+      this.utils.showAlert('Informe o Estado', "Campo Vazio!");
+    } 
   }
 }
